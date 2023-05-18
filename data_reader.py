931346217c88291f5e623daace4e574d0cd358c3
andynice <ancacoch@gmail.com>
2023-05-18 19:19:33 +0200

    Drop tweet column Fix issues with logic to handle the dates files

--- data_reader.py
+++ data_reader.py
@@ -19,7 +19,7 @@ def read_date_file(date):
 def filter_dataframe(df_data, langs):
 
     filtered_df = df_data[df_data["lang"].isin(langs)]
-    filtered_df = df_data.drop(["geo", "id", "source"], axis=1)
+    filtered_df = df_data.drop(["geo", "id", "source"], axis=1, errors="ignore")
 
     # Convert "created_at" column to datetime
     filtered_df['created_at'] = pd.to_datetime(filtered_df['created_at'])
@@ -28,7 +28,7 @@ def filter_dataframe(df_data, langs):
     # data['created_at'] = data['created_at'].dt.strftime('%Y-%m-%d %H:00:00')
     filtered_df['created_at'] = filtered_df['created_at'].dt.strftime('%Y-%m-%d %H')
 
-    return [filtered_df["lang"] == lang for lang in langs]
+    return [filtered_df[filtered_df["lang"] == lang] for lang in langs]
 
 ## CLEAN TEXT
 en_stopwords = ["able","about","above","abroad","according","accordingly","across","actually","adj","after","afterwards","again","against","ago","ahead","ain't","all","allow","allows","almost","alone","along","alongside","already","also","although","always","am","amid","amidst","among","amongst","an","and","another","any","anybody","anyhow","anyone","anything","anyway","anyways","anywhere","apart","appear","appreciate","appropriate","are","aren't","around","as","a's","aside","ask","asking","associated","at","available","away","awfully","back","backward","backwards","be","became","because","become","becomes","becoming","been","before","beforehand","begin","behind","being","believe","below","beside","besides","best","better","between","beyond","both","brief","but","by","came","can","cannot","cant","can't","caption","cause","causes","certain","certainly","changes","clearly","c'mon","co","co.","com","come","comes","concerning","consequently","consider","considering","contain","containing","contains","corresponding","could","couldn't","course","c's","currently","dare","daren't","definitely","described","despite","did","didn't","different","directly","do","does","doesn't","doing","done","don't","down","downwards","during","each","edu","eg","eight","eighty","either","else","elsewhere","end","ending","enough","entirely","especially","et","etc","even","ever","evermore","every","everybody","everyone","everything","everywhere","ex","exactly","example","except","fairly","far","farther","few","fewer","fifth","first","five","followed","following","follows","for","forever","former","formerly","forth","forward","found","four","from","further","furthermore","get","gets","getting","given","gives","go","goes","going","gone","got","gotten","greetings","had","hadn't","half","happens","hardly","has","hasn't","have","haven't","having","he","he'd","he'll","hello","help","hence","her","here","hereafter","hereby","herein","here's","hereupon","hers","herself","he's","hi","him","himself","his","hither","hopefully","how","howbeit","however","hundred","i'd","ie","if","ignored","i'll","i'm","immediate","in","inasmuch","inc","inc.","indeed","indicate","indicated","indicates","inner","inside","insofar","instead","into","inward","is","isn't","it","it'd","it'll","its","it's","itself","i've","just","k","keep","keeps","kept","know","known","knows","last","lately","later","latter","latterly","least","less","lest","let","let's","like","liked","likely","likewise","little","look","looking","looks","low","lower","ltd","made","mainly","make","makes","many","may","maybe","mayn't","me","mean","meantime","meanwhile","merely","might","mightn't","mine","minus","miss","more","moreover","most","mostly","mr","mrs","much","must","mustn't","my","myself","name","namely","nd","near","nearly","necessary","need","needn't","needs","neither","never","neverf","neverless","nevertheless","new","next","nine","ninety","no","nobody","non","none","nonetheless","noone","no-one","nor","normally","not","nothing","notwithstanding","novel","now","nowhere","obviously","of","off","often","oh","ok","okay","old","on","once","one","ones","one's","only","onto","opposite","or","other","others","otherwise","ought","oughtn't","our","ours","ourselves","out","outside","over","overall","own","particular","particularly","past","per","perhaps","placed","please","plus","possible","presumably","probably","provided","provides","que","quite","qv","rather","rd","re","really","reasonably","recent","recently","regarding","regardless","regards","relatively","respectively","right","round","said","same","saw","say","saying","says","second","secondly","see","seeing","seem","seemed","seeming","seems","seen","self","selves","sensible","sent","serious","seriously","seven","several","shall","shan't","she","she'd","she'll","she's","should","shouldn't","since","six","so","some","somebody","someday","somehow","someone","something","sometime","sometimes","somewhat","somewhere","soon","sorry","specified","specify","specifying","still","sub","such","sup","sure","take","taken","taking","tell","tends","th","than","thank","thanks","thanx","that","that'll","thats","that's","that've","the","their","theirs","them","themselves","then","thence","there","thereafter","thereby","there'd","therefore","therein","there'll","there're","theres","there's","thereupon","there've","these","they","they'd","they'll","they're","they've","thing","things","think","third","thirty","this","thorough","thoroughly","those","though","three","through","throughout","thru","thus","till","to","together","too","took","toward","towards","tried","tries","truly","try","trying","t's","twice","two","un","under","underneath","undoing","unfortunately","unless","unlike","unlikely","until","unto","up","upon","upwards","us","use","used","useful","uses","using","usually","v","value","various","versus","very","via","viz","vs","want","wants","was","wasn't","way","we","we'd","welcome","well","we'll","went","were","we're","weren't","we've","what","whatever","what'll","what's","what've","when","whence","whenever","where","whereafter","whereas","whereby","wherein","where's","whereupon","wherever","whether","which","whichever","while","whilst","whither","who","who'd","whoever","whole","who'll","whom","whomever","who's","whose","why","will","willing","wish","with","within","without","wonder","won't","would","wouldn't","yes","yet","you","you'd","you'll","your","you're","yours","yourself","yourselves","you've","zero","a","how's","i","when's","why's","b","c","d","e","f","g","h","j","l","m","n","o","p","q","r","s","t","u","uucp","w","x","y","z","I","www","amount","bill","bottom","call","computer","con","couldnt","cry","de","describe","detail","due","eleven","empty","fifteen","fifty","fill","find","fire","forty","front","full","give","hasnt","herse","himse","interest","itse”","mill","move","myse”","part","put","show","side","sincere","sixty","system","ten","thick","thin","top","twelve","twenty","abst","accordance","act","added","adopted","affected","affecting","affects","ah","announce","anymore","apparently","approximately","aren","arent","arise","auth","beginning","beginnings","begins","biol","briefly","ca","date","ed","effect","et-al","ff","fix","gave","giving","heres","hes","hid","home","id","im","immediately","importance","important","index","information","invention","itd","keys","kg","km","largely","lets","line","'ll","means","mg","million","ml","mug","na","nay","necessarily","nos","noted","obtain","obtained","omitted","ord","owing","page","pages","poorly","possibly","potentially","pp","predominantly","present","previously","primarily","promptly","proud","quickly","ran","readily","ref","refs","related","research","resulted","resulting","results","run","sec","section","shed","shes","showed","shown","showns","shows","significant","significantly","similar","similarly","slightly","somethan","specifically","state","states","stop","strongly","substantially","successfully","sufficiently","suggest","thered","thereof","therere","thereto","theyd","theyre","thou","thoughh","thousand","throug","til","tip","ts","ups","usefully","usefulness","'ve","vol","vols","wed","whats","wheres","whim","whod","whos","widely","words","world","youd","youre"]
@@ -42,9 +42,7 @@ def tokenize_words(text):
 def clean_text(data, language):
 
     results = []
-    for row in data:
-        text = row[10]
-
+    for text in data:
         # remove emojis
         text = demoji.replace(text, repl="")
 
@@ -67,26 +65,29 @@ def clean_text(data, language):
         text = re.sub('[!,*)@#%(&$_?.^:]', '', text)
 
         # Lemmatization and remove stopwords
-        if language == "english":
+        if language == "en":
             stop_words = en_stopwords
             words = tokenize_words(text.lower())
             words = [WordNetLemmatizer().lemmatize(word) for word in words if word not in (stop_words)]
-        elif language == "german":
+        elif language == "de":
             stop_words = de_stopwords
             nlp = spacy.load('de_core_news_sm')
             lemma_text = nlp(text)
             words = [word.lemma_.lower() for word in lemma_text if word.lemma_ not in (stop_words) and not re.match(r'\b\d+\b', word.lemma_)]
+
         results.append(words)
 
     return results
 
 ## GENERATE FRAQUENCIES
 def count_frequencies(text):
+    print(f"text to count freqs:{text}")
     return text
 
 file_exists = [False, False]
 # Output CSV file
-date_ranges = [['2021-01-03', '2021-01-04'], ['2021-02-01', '2021-02-03']]
+date_ranges = [['2021-01-04', '2021-01-05'], ['2021-02-04', '2021-02-05']]
+
 languages = ['en', 'de']
 
 for date_range in date_ranges:
@@ -97,14 +98,18 @@ for date_range in date_ranges:
 
     for date in date_generated:
         date_str = date.strftime("%Y-%m-%d")
-        print(date_str)
         df_data = read_date_file(date_str)
-        df_data = filter_dataframe(df_data, languages)
-        for lang_idx, df_data in enumerate(df_data):
-            print(df_data.head(10))
+        filtered_dfs = filter_dataframe(df_data, languages)
+        for lang_idx, df_data in enumerate(filtered_dfs):
+            print(f"Processing file: {date_str} / language: {languages[lang_idx]}")
+            # print("df_data.head(5)")
+            # print(df_data.head(5))
 
             # Clean text column
             df_data['clean_tweets'] = clean_text(df_data['tweet'], languages[lang_idx])
+            print("df_data['clean_tweets']")
+            print(df_data['clean_tweets'])
+            print(df_data.head(5))
 
             # Drop tweet column
             df_data = df_data.drop(["tweet"], axis=1)
@@ -112,10 +117,10 @@ for date_range in date_ranges:
             # Count Frequencies
             df_data['freq_count'] = count_frequencies(df_data['clean_tweets'])
 
-            output_file = f"output_{languages[lang_idx]}.csv"
-
+            output_file = f"output_{date_str}_{languages[lang_idx]}.csv"
+            print(f"saved file {output_file}")
             # Write processed data to output file
-            df_data.to_csv(output_file, mode='a', index=False, header=not file_exists[lang_idx])
+            #df_data.to_csv(output_file, mode='a', index=False, header=not file_exists[lang_idx])
 
             # Set file_exists to True after writing the header once
             file_exists[lang_idx] = True
