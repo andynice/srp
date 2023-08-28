eac28e073ae479d7058250cfd9f7b493c63751f2
andynice <ancacoch@gmail.com>
2023-08-28 19:59:27 +0200

    Add test.sh for submitting a batch script

---
+++ word2vec_training/test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=test
+#SBATCH --output=test%j.log
+#SBATCH --error=test%j.err
+#SBATCH --mail-user=correa@uni-hildesheim.de
+#SBATCH --partition=STUD
+#SBATCH --gres=gpu:1
+
+echo "This is a test echo"
+srun /home/correa/miniconda3/envs/word2vec/bin/python word_embedding.py        # python jobs require the srun command to work
