fa2792e359e07f6b3436cf4b721cdb240bb2ad49
andynice <ancacoch@gmail.com>
2023-10-14 18:05:42 +0200

    Prepare download for model covid bert v2

--- model_bert_training/download_bert_model.py
+++ model_bert_training/download_bert_model.py
@@ -1,10 +1,14 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, DataCollatorWithPadding
 
 # MODEL DEFINITION
-BASE_MODEL = "digitalepidemiologylab/covid-twitter-bert"
+# BASE_MODEL = "digitalepidemiologylab/covid-twitter-bert"
+BASE_MODEL = "digitalepidemiologylab/covid-twitter-bert-v2"
 
 tokenizer = AutoTokenizer.from_pretrained(BASE_MODEL)
 model = AutoModelForSequenceClassification.from_pretrained(BASE_MODEL, num_labels=1)
 
-tokenizer.save_pretrained("./covid-twitter-bert")
-model.save_pretrained("./covid-twitter-bert")
+# tokenizer.save_pretrained("./covid-twitter-bert")
+# model.save_pretrained("./covid-twitter-bert")
+
+tokenizer.save_pretrained("./covid-twitter-bert-v2")
+model.save_pretrained("./covid-twitter-bert-v2")
