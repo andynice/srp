5e1b34b143f76c55ecb4928e139d31133cbf7015
andynice <ancacoch@gmail.com>
2023-10-16 01:26:05 +0200

    Update to use only 1 gpu

--- model_bert_training/test.sh
+++ model_bert_training/test.sh
@@ -4,7 +4,7 @@
 #SBATCH --error=test%j.err
 #SBATCH --mail-user=correa@uni-hildesheim.de
 #SBATCH --partition=STUD
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 
 echo "Before running model_covid_twitter_bert_training.py"
 srun /home/correa/miniconda3/envs/model_bert_training/bin/python model_covid_twitter_bert_training.py --train True       # python jobs require the srun command to work
